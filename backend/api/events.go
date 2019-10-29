3e4ecc73fa7f67f3f4ed3d971d0a3ce50bb40fdb
Unknown <particleman.smith@gmail.com>
2019-10-29 00:32:32 -0600

    zpool state event working

--- backend/api/events.go
+++ backend/api/events.go
@@ -60,21 +60,20 @@ func Error(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	send := true // Whether or not to send the message (body contents may warrant not sending a message)
-	msg := "ERROR\n"
+	msg := "WARNING\n"
 
 	// Error types
 	switch request.URL.Path {
 	// Bash
 	case "/bash-event/exec-failure":
-		// TODO: Get more information from request about which script was run (should be sent by the Python script)
 		script := bodyMap["script"]
 		msg += "I encountered a failure running the Python script `" + fmt.Sprintf("%v", script) + "`."
 	// ZFS
 	case "/zfs-event/data-error":
 		msg += "I noticed data corruption on a ZFS vdev."
 	case "/zfs-event/zpool-state":
-		// TODO: Use body
-		msg += "A disk in a ZFS vdev has gone unavailable!"
+		state := fmt.Sprintf("%v", bodyMap["status"])
+		msg += "A disk in a ZFS vdev has entered " + AOrAn(state) + state + " state!"
 	// Backup
 	case "/backup-event/failure":
 		msg += "I encountered a failure backing up /home, /etc, or /var."
@@ -91,5 +90,27 @@ func Error(writer http.ResponseWriter, request *http.Request) {
 			json.NewEncoder(writer).Encode(returnMsg)
 		}
 	}
+}
 
+/*
+IsFirstCharVowel checks if the first character of a word is a vowel and returns true or false.
+*/
+func IsFirstCharVowel(word string) bool {
+	vowels := []byte{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
+	for _, v := range vowels {
+		if word[0] == v {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+AOrAn will "a" or "an" based on whether or not the first character of the given word is a vowel.
+*/
+func AOrAn(word string) string {
+	if IsFirstCharVowel(word) {
+		return "an "
+	}
+	return "a "
 }
